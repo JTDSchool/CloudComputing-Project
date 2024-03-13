797d70fabe03d3ed57aa25f26e71f0b3a2efb906
JarredTD <jarred.desrosiers@yahoo.com>
2024-03-13 07:15:09 -0400

    feat: Call cloud function with api and api key

--- app/app.py
+++ app/app.py
@@ -1,4 +1,7 @@
+# pylint: disable=consider-using-from-import
 from flask import Flask, render_template, request
+import pip._vendor.requests as requests
+from .constants import API_KEY
 
 app = Flask(__name__)
 
@@ -10,8 +13,22 @@ def index():
 
 @app.route("/summarize", methods=["POST"])
 def summarize():
-    text = request.form["text"]
-    summarized_text = text
+    api_url = "https://call-model-gateway-58u11f9h.uc.gateway.dev/call_model"
+    api_key = API_KEY
+    data = {"text": request.form["text"]}
+
+    response = requests.post(f"{api_url}?key={api_key}", json=data)
+
+    if response.status_code == 200:
+        response_json = response.json()
+
+        summarized_text = response_json.get("text", "")
+
+    else:
+        summarized_text = (
+            f"There was an error -> {response.status_code}: {response.text}"
+        )
+
     return render_template("result.html", summarized_text=summarized_text)
 
 
