662cee0216a5f1042e79c3e2621309a3c7eaa04d
JarredTD <jarred.desrosiers@yahoo.com>
2024-03-21 02:26:15 -0400

    build: Remove deploy-function script

--- scripts/deploy-functions.sh
+++
@@ -1,8 +0,0 @@
-cd ./cloud-functions
-
-gcloud functions deploy call_model \
-    --runtime python311 \
-    --trigger-http \
-    --entry-point=call_model \
-    --source=. \
-    --no-allow-unauthenticated
